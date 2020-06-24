37a33f76c59f5da7c04316fc8d5da8a00eadd40e
Daphne <daphne@vlhc.nl>
2020-06-24 10:18:00 +0200

    second commit with list

---
+++ ToDoList.tsx
@@ -0,0 +1,13 @@
+import * as React from "react";
+import "./ToDoList.css";
+
+export const ToDoList = () => {
+  return (
+    <div className="List">
+      <ul>
+        <li>a</li>
+        <li>b</li>
+      </ul>
+    </div>
+  );
+};
--- ToDoListBlock.tsx
+++ ToDoListBlock.tsx
@@ -1,10 +1,12 @@
 import * as React from "react";
 import "./ToDoListBlock.css";
+import { ToDoList } from "./ToDoList";
 
 export const ToDoListBlock = () => {
-    return (
-      <div className="Block">
-        <input type="text" id="InputTask" name="phone" placeholder="What needs to be done?" />
-      </div>
-    );
-  };
+  return (
+    <div className="Block">
+      <input type="text" id="InputTask" placeholder="What needs to be done?" />
+      <ToDoList />
+    </div>
+  );
+};
